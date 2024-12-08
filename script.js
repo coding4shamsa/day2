0fe7c4b6db97038d5b8905f4b6838ae9c7c74053
Shamsa <codingshamsa@gmail.com>
2024-12-08 10:40:28 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+//Operators
+//Arithmetic operator
+ let x = '5-2';
+ console.log(x);
+
+ //addition
+ let a = '5+3';
+ console.log(a);
+ //multiply
+ let b = '2*2';
+ console.log(b)
+
+ //assignment oberator
+ let y = 5;
+ y+=4;
+ console.log(y);
+
+ //loops
+ //for loop
+ for(i=0; i<=10; i++){
+    console.log("shamsa")
+ }
+
+ //for loop array
+
+ let personc = ["ali", "ahmed", "ayaan"]
+
+ for(i=1; i=person.length; i++){
+    console.log(person[i])
+ }
+
+ //while loop
+
+ let i =1;
+ while(i<=10){
+    console.log(i);
+    i++;
+ }
+ 
+ //do while loop
+
+ let j =1;
+ do{
+    console.log(j);
+    j++;
+
+ }while(j<=5);
+
+ //break / continue
+ for(let i=1; i<=5; i++){
+    if(i==3){
+        break;
+    }
+    console.log(i);
+ }
